673df2fd8ee006165981156140d909abb71bdfe5
niteesh6024 <85555871+niteesh6024@users.noreply.github.com>
2023-09-09 11:46:22 +0530

    Create infixToPostfix_associativity.java

---
+++ stack/infixToPostfix_associativity.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        infixToPostfix("((a+b)*c)-d^e^f");
+    }
+    public static void infixToPostfix(String exp){
+        ArrayDeque<Character> stack=new ArrayDeque<>();
+        String postfix="";
+        HashMap<Character,Integer> inStackPrecedence=new HashMap<>();
+        HashMap<Character,Integer> outStackPrecedence=new HashMap<>();
+        outStackPrecedence.put('+',1);inStackPrecedence.put('+',2);
+        outStackPrecedence.put('-',1);inStackPrecedence.put('-',2);
+        outStackPrecedence.put('*',3);inStackPrecedence.put('*',4);
+        outStackPrecedence.put('/',3);inStackPrecedence.put('/',4);
+        outStackPrecedence.put('^',6);inStackPrecedence.put('^',5);
+        outStackPrecedence.put('(',7);inStackPrecedence.put('(',0);
+        outStackPrecedence.put(')',0);inStackPrecedence.put(')',-1);
+        int i=0;
+        while (i<exp.length()){
+            if(checkKey(exp.charAt(i),inStackPrecedence)){
+                if(stack.isEmpty()||outStackPrecedence.get(exp.charAt(i))>inStackPrecedence.get(stack.peek())){
+                    stack.push(exp.charAt(i++));
+                }
+                else if(outStackPrecedence.get(exp.charAt(i))==inStackPrecedence.get(stack.peek())){
+                    stack.pop();
+                    i++;
+                }
+                else{
+                    postfix+=stack.pop();
+                }
+            }
+            else{
+                postfix+=exp.charAt(i++);
+            }
+        }
+        while (!stack.isEmpty()){
+            postfix+=stack.pop();
+        }
+        System.out.println(postfix);
+    }
+    public static boolean checkKey(char c, HashMap<Character,Integer> precedence){
+        for(Map.Entry<Character,Integer> l:precedence.entrySet()){
+            if(l.getKey()==c){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
