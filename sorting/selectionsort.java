cc913632b62c86e5f2e2ab357a8f4fcf92613adc
niteesh6024 <85555871+niteesh6024@users.noreply.github.com>
2023-10-09 22:48:51 +0530

    Update selectionsort.java

--- sorting/selectionsort.java
+++ sorting/selectionsort.java
@@ -8,7 +8,7 @@ bubble sort:
 import java.util.*;
 
 public class Main {
-    public static void bubbleSort(int[] n,int s) {
+    public static void selectionSort(int[] n,int s) {
         int k,t;
         for(int i=0;i<s;i++){
             k=i;
@@ -23,7 +23,7 @@ public class Main {
     }
     public static void main(String[] args) {
         int n[]=new int[]{5,2,7,6,4,3,6};
-        bubbleSort(n,n.length);
+        selectionSort(n,n.length);
         for(int i:n){
             System.out.println(i);
         }
